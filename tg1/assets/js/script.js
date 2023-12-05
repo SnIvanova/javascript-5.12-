69f767b3ea6291b037f70bd34727dc2dff4d3173
SnIvanova <1snizhanaivanova@gmail.com>
2023-12-05 10:33:51 +0100

    Initial commit

---
+++ tg1/assets/js/script.js
@@ -0,0 +1,33 @@
+alert("hi");
+
+// 1. Create a function called "myFunction" that takes in two parameters: "x" and "y". 
+function myFunction(x, y) {}
+    // 2. Inside the function, use an if statement to check if x is greater than or equal to y. If it is, return 
+
+
+
+// Varibili JS
+let var1 = 'testo';
+let var2 = 25;
+let var3 = true;
+let var4 = [];
+let var5 = {};
+let var6 = undefined;
+let var7 = null;
+
+// Funzioni JS
+// dichiarazione di funzione
+func1();
+function func1(params) {
+    // blocco di istruzioni
+    return params; 
+}// ritorno un valore
+ 
+    
+//func2(); //errore
+//funzione espressione
+let func2 = function(params) {
+// blocco di istruzioni
+    return params;// ritorno un valore
+} 
+func2();
